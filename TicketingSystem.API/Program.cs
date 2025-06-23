534c9464c04f9b1f2101072a92de81777528670f
Oday Alsanad <odayalsanad6374@gmail.com>
2025-06-24 00:10:20 +0300

    UseSwagger

--- TicketingSystem.API/Program.cs
+++ TicketingSystem.API/Program.cs
@@ -1,3 +1,4 @@
+using Microsoft.OpenApi.Models;
 
 namespace TicketingSystem.API
 {
@@ -7,27 +8,34 @@ namespace TicketingSystem.API
         {
             var builder = WebApplication.CreateBuilder(args);
 
-            // Add services to the container.
-
+            // ? Add services to the container.
             builder.Services.AddControllers();
-            // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
-            builder.Services.AddOpenApi();
+            builder.Services.AddEndpointsApiExplorer(); // ??? ????
+            builder.Services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo
+                {
+                    Title = "TicketingSystem API",
+                    Version = "v1"
+                });
+            });
 
             var app = builder.Build();
 
-            // Configure the HTTP request pipeline.
+            // ? Configure the HTTP request pipeline.
             if (app.Environment.IsDevelopment())
             {
-                app.MapOpenApi();
+                app.UseSwagger();
+                app.UseSwaggerUI(c =>
+                {
+                    c.SwaggerEndpoint("/swagger/v1/swagger.json", "TicketingSystem API v1");
+                    c.RoutePrefix = "swagger"; // ???? ??? /swagger
+                });
             }
 
             app.UseHttpsRedirection();
-
             app.UseAuthorization();
-
-
             app.MapControllers();
-
             app.Run();
         }
     }
